2955435d74cea35edd3f89996e6cad3b12cc44a3
Tuffy90 <misha.gvozdenko11@gmail.com>
2026-02-19 12:58:56 +0100

    C Declare Multiple Variables
    Fix variable declarations and update print statements.

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main() {
-  int = MIBOMBO;
-  MIBOMBO = 100
-  printf("MIBOMBO");
+  int x = 2, y = 3 , z = 4;
+  int YASIN = 12;
+  int MIBOMBO;
+  MIBOMBO = 100;
+  printf("MIBOMBO = %d", MIBOMBO + YASIN);
+  printf("\nYASIN BOOM\n");
+  printf("SUMA =%d ",x + y + z );
   return 0;
 }
